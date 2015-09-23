7b1c707cbcb830a3c6a13ae9f6ebbba95c55eab8
Quincy Larson <michaelqlarson@gmail.com>
2015-09-23 15:22:30 -0700

    start modding csp - still need to test and cull trusted

--- server/middlewares/csp.js
+++ server/middlewares/csp.js
@@ -9,85 +9,53 @@ const trusted = [
   'http://freecodecamp.com',
   'https://www.freecodecamp.com',
   'https://freecodecamp.com',
-  'https://freecodecamp.org',
-  '*.freecodecamp.org',
-  // NOTE(berks): add the following as the blob above was not covering www
-  'http://www.freecodecamp.org',
   'ws://freecodecamp.com/',
   'ws://www.freecodecamp.com/',
-  '*.gstatic.com',
   '*.google-analytics.com',
-  '*.googleapis.com',
   '*.google.com',
   '*.gstatic.com',
   '*.doubleclick.net',
   '*.twitter.com',
   '*.twitch.tv',
-  '*.twimg.com',
   "'unsafe-eval'",
   "'unsafe-inline'",
   '*.bootstrapcdn.com',
   '*.cloudflare.com',
-  'https://*.cloudflare.com',
   'localhost:3001',
   'ws://localhost:3001/',
   'http://localhost:3001',
   'localhost:3000',
   'ws://localhost:3000/',
   'http://localhost:3000',
-  '*.ionicframework.com',
-  'https://syndication.twitter.com',
   '*.youtube.com',
   '*.jsdelivr.net',
   'https://*.jsdelivr.net',
-  '*.ytimg.com',
-  '*.bitly.com',
-  'http://cdn.inspectlet.com/',
-  'https://cdn.inspeclet.com/',
-  'wss://inspectletws.herokuapp.com/',
-  'http://hn.inspectlet.com/',
-  '*.googleapis.com',
-  '*.gstatic.com',
-  'https://hn.inspectlet.com/'
+  '*.bitly.com'
 ];
 
 export default function csp() {
   return helmet.csp({
     defaultSrc: trusted,
     scriptSrc: [
-      '*.optimizely.com',
-      '*.aspnetcdn.com',
-      '*.d3js.org',
-      'https://cdn.inspectlet.com/inspectlet.js',
-      'http://cdn.inspectlet.com/inspectlet.js',
-      'http://beta.freecodecamp.com'
+      '*.optimizely.com'
     ].concat(trusted),
     'connect-src': [
-      'vimeo.com'
+      '*.vimeo.com'
     ].concat(trusted),
-    styleSrc: [
-      '*.googleapis.com',
-      '*.gstatic.com'
-    ].concat(trusted),
+    styleSrc: [].concat(trusted),
     imgSrc: [
       // allow all input since we have user submitted images for
       // public profile
       '*'
     ].concat(trusted),
-    fontSrc: [
-      '*.googleapis.com',
-      '*.gstatic.com'
-    ].concat(trusted),
+    fontSrc: [].concat(trusted),
     mediaSrc: [
       '*.amazonaws.com',
       '*.twitter.com'
     ].concat(trusted),
     frameSrc: [
-      '*.gitter.im',
-      '*.gitter.im https:',
       '*.vimeo.com',
-      '*.twitter.com',
-      '*.ghbtns.com'
+      '*.twitter.com'
     ].concat(trusted),
     // set to true if you only want to report errors
     reportOnly: false,
